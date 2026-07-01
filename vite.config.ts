c780c4378cef8a1af5ee670cf0d3f45484609118
Giorgi Shalvashvili <george0shalvashvili@gmail.com>
2026-07-01 18:00:22 +0400

    setup pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/pomodoro_html/',
   plugins: [react(), tailwindcss()],
 })
